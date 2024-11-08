40164fc27e98db0d526a582e18311f2f05f99f60
mdizon00 <m.dizon1@rgu.ac.uk>
2024-11-08 16:20:56 +0000

    Added particles on collision

---
+++ Assets/Scripts/ObjectCollision.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollision : MonoBehaviour
+{
+
+    [SerializeField] ParticleSystem particle;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if(collision.contacts[0].point.y > transform.position.y && collision.collider.tag == "Player")
+        {
+            Destroy(gameObject);
+            particle = Instantiate(particle, transform.position, transform.rotation);
+            particle.Play();
+        }
+    }
+
+
+}
