5c78edfb092413d8dea0087ee825af3883860fdb
mdizon00 <m.dizon1@rgu.ac.uk>
2024-11-14 11:33:05 +0000

    Added Physics Based Platform

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+
+    private float fallDelay = 1f;
+    private float destroyDelay = 2f;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.contacts[0].point.y > transform.position.y && collision.collider.tag == "Player")
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+    private IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        Destroy(gameObject, destroyDelay);
+    }
+
+}
